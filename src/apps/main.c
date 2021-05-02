43c2fa8a7d45c44460e5c401ac9505960e0b3718
Biradar Nandkishor <nandkishor.biradar@danfoss.com>
2021-05-02 18:57:15 +0530

    Command line argument support is added

--- src/apps/main.c
+++ src/apps/main.c
@@ -21,23 +21,99 @@
 #include "elements/xs_schema.h"
 #include "apps/xsd.h"
 #include "code_generator.h"
+#include "cargs.h"
 
 /*
  *  ------------------------------ FUNCTION BODY ------------------------------
  */
 
+/*
+ * i, input      : input source file
+ * a, allocate   : Allocation method for multiple XML elements.
+ *                 + Array: use array to store content of multiple XML elements
+ *                 + Dynamic: use allocator function to allocate memory dynamically to store multiple XML elements
+ * content callback
+ * Contenxt
+ *
+*/
+
+/**
+ * This is the main configuration of all options available.
+ */
+static struct cag_option Cag_Options[] = {
+  {
+    .identifier = 'd',
+    .access_letters = "dD",
+    .access_name = "dynamic",
+    .value_name = NULL,
+    .description = "Uses allocate callback function to allow user to allocate memory dynamically of multiple occurrence of XML elements."
+  },
+  {
+    .identifier = 'c',
+    .access_letters = "cC",
+    .access_name = "context",
+    .value_name = NULL,
+    .description = "Enable context"
+  },
+  {
+    .identifier = 'b',
+    .access_letters = "bB",
+    .access_name = "callback",
+    .value_name = NULL,
+    .description = "Enable callback for each XML element."
+  },
+  {
+    .identifier = 'h',
+    .access_letters = "h",
+    .access_name = "help",
+    .description = "Shows the command help"
+  }
+};
+
 int main(int argc, char *argv[])
 {
-  if(argc < 2)
+  cag_option_context context;
+
+   if(argc < 2)
+    {
+      printf("Usage: XML code generator [OPTION] input.xsd \n");
+      cag_option_print(Cag_Options, CAG_ARRAY_SIZE(Cag_Options), stdout);
+      return EXIT_FAILURE;
+    }
+
+  // Initialize options
+  options_t options = {.Occurrence = ARRAY, .Context = "", .Content_Callback = false};
+  cag_option_prepare(&context, Cag_Options, CAG_ARRAY_SIZE(Cag_Options), argc, argv);
+  while(cag_option_fetch(&context))
   {
-    printf("Please enter the schema file name\n");
-    return 1;
+    switch(cag_option_get(&context))
+    {
+    case 'd':
+      options.Occurrence = DYNAMIC;
+      break;
+
+    case 'c':
+      options.Context = CONTEXT_STRING;
+      break;
+
+    case 'b':
+      options.Content_Callback = true;
+      break;
+
+    default:
+      printf("Unknown argument passed.\n");
+
+    case 'h':
+      printf("Usage: XML code generator [OPTION] input.xsd \n");
+      cag_option_print(Cag_Options, CAG_ARRAY_SIZE(Cag_Options), stdout);
+      return EXIT_SUCCESS;
+    }
   }
 
-  FILE* fSchema = fopen(argv[1], "r");
+  FILE* fSchema = fopen(argv[argc - 1], "r");
   if(fSchema == NULL )
   {
-    printf("Error in opening XML schema file\n");
+    printf("Error in opening XML schema file: %s\n", argv[argc - 1]);
     return 2;
   }
 
@@ -53,7 +129,6 @@ int main(int argc, char *argv[])
   if(result == XML_PARSE_SUCCESS)
   {
     printf("Parsing completed successfully\n");
-    options_t options = {.Occurrence = ARRAY, .Context = ", void** context", .Content_Callback = false};
     const xs_element_t* root = compile_xsd(pXsdData->Descendant, &options);
     generate_xml_source(root, &options);
     generate_print_file(root);
@@ -64,7 +139,6 @@ int main(int argc, char *argv[])
   }
 
   free(schema);
-
   return 0;
 }
 
