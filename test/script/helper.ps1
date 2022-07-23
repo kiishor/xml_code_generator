1af0e931a0a9546b919e5b1ea3759ce22bdbad40
Nandkishor Biradar <kiishor@icloud.com>
2022-07-23 20:47:31 +0530

    Added script to validate XML code generator to make sure that it doesn't break existing code generation.

---
+++ test/script/helper.ps1
@@ -0,0 +1,72 @@
+﻿function compareFile 
+{
+    param ($source, $target)
+
+    # Check if reference source file exists
+    if(-not(Test-Path -Path $source -PathType Leaf)) 
+    {
+        echo "Reference source file does not exist: $referenceSource"    
+        exit 2
+    }
+
+    # Check if target source file exists
+    if(-not(Test-Path -Path $target -PathType Leaf)) 
+    {
+        echo "target source file does not exist: $targetSource"    
+        exit 3
+    }
+
+    $old = Get-Content $source -raw
+    $new = Get-Content $target -raw
+
+    if(-not($old -ceq $new))
+    {
+        echo "source file doesn't match with target file"
+        echo $source
+        exit 4
+    }
+}
+
+function generateSource
+{
+    param ($tool, $referencePath, $targetPath, $name, $options, $source)
+    
+    $schema = "$referencePath\$schema.xsd"
+
+    echo "Parsing XML schema: $schema"
+
+    # Remove target path if it already exists
+    if(Test-Path -Path $targetPath) 
+    {
+        Remove-Item $targetPath -Recurse -Force
+    }
+
+    New-Item -Path $targetPath -ItemType Directory        
+    cd $targetPath
+
+    # Execute xml_code_generator to parse reference schema file and generate source code
+    echo "$tool $options $schema"
+
+    & $tool $options $schema 
+    if($LastExitCode -ne 0)
+    {
+        echo "Failed to genrate source code. "
+        exit 1
+    }
+
+    if(!$source)
+    {
+        $source = $name
+    }
+
+    # Compare generated source
+    $referenceSource = "$referencePath\$source.c"
+    $targetSource = "$targetPath\$source.c"
+    compareFile -source $referenceSource -target $targetSource
+
+    # Compare generated header
+    $referenceHeader = "$referencePath\$source.h"
+    $targetHeader = "$targetPath\$source.h"
+    compareFile -source $referenceHeader -target $targetHeader
+    cd ../..
+}
---
+++ test/script/validate.ps1
@@ -0,0 +1,55 @@
+﻿$ErrorActionPreference = "Stop"
+
+$tool = 'C:\project\xml_code_generator\workspace\codeblocks\bin\Debug\xml_code_generator.exe'
+
+. "$PSScriptRoot\helper.ps1"
+
+function validateTool
+{
+    param($schema, $option, $path, $source)
+
+    if(!$path)
+    {
+        $path = $schema
+    }
+
+    echo "path: $path, schema: $schema"
+    
+    generateSource -tool $tool -referencePath "$pwd/reference/$path" -targetPath "$pwd/generated/$path" -name $schema -option $option -source $source
+    if($LastExitCode -eq 0)
+    {
+        echo "Successfully validated for $schema.xsd"
+    }
+    else
+    {
+        Write-Error "Failed to validate xml_generator_tool for $schema.xsd"
+        exit 1
+    }
+    echo ""
+}
+
+function setup
+{
+    cd ..
+    if(Test-Path 'generated') 
+    {
+        Remove-Item 'generated' -Recurse -Force
+    }
+    New-Item -Path 'generated' -ItemType Directory
+}
+
+setup
+validateTool -schema "food"
+validateTool -schema "purchase_order" "-b" -source "PurchaseOrder"
+
+validateTool -schema "shiporder1" "-b" -path "shiporder1" -source "shiporder"
+validateTool -schema "shiporder2" "-b" -path "shiporder1" -source "shiporder"
+validateTool -schema "shiporder3" "-b" -path "shiporder1" -source "shiporder"
+
+validateTool -schema "shiporder1" @("-b", "-d") -path "shiporder2" -source "shiporder"
+validateTool -schema "shiporder2" @("-b", "-d") -path "shiporder2" -source "shiporder"
+validateTool -schema "shiporder3" @("-b", "-d") -path "shiporder2" -source "shiporder"
+
+validateTool -schema "shiporder1" @("-d", "-c") -path "shiporder3" -source "shiporder"
+validateTool -schema "shiporder2" @("-d", "-c") -path "shiporder3" -source "shiporder"
+validateTool -schema "shiporder3" @("-d", "-c") -path "shiporder3" -source "shiporder"
